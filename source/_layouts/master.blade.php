77534eb3397eab98bb8ccb62d727b7b0b9bcc8da
Dieter Stinglhamber <elhebert@hotmail.be>
2018-12-21 07:22:10 +0100

    (feat): remove google font dependency

--- source/_layouts/master.blade.php
+++ source/_layouts/master.blade.php
@@ -5,10 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta http-equiv="x-ua-compatible" content="ie=edge">
         <link rel="stylesheet" href="/assets/build/css/hljs.css">
-        {{-- <link rel="stylesheet" href="{{ mix('css/fonts.css', 'assets/build') }}"> --}}
-
-        <link href="https://fonts.googleapis.com/css?family=Fira+Sans:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i|Montserrat:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
-
+        <link rel="stylesheet" href="{{ mix('css/fonts.css', 'assets/build') }}">
         <link rel="stylesheet" href="{{ mix('css/main.css', 'assets/build') }}">
 
         <title>Dieter Stinglhamber</title>
