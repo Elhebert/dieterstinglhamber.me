d45ea0e6f4a3e97a054c3a914d7c39e4733ef2cc
Dieter Sting <3165079+Elhebert@users.noreply.github.com>
2018-12-28 07:11:46 +0100

    (feat): add 404 page (#12)

---
+++ source/404.blade.php
@@ -0,0 +1,22 @@
+---
+permalink: 404.html
+---
+
+@extends('_layouts.master')
+
+@section('body')
+<main class="flex-grow | pt-16 lg:flex lg:flex-col lg:justify-center | container mx-auto | text-center">
+    <div class="mx-4 lg:mx-auto max-w-lg">
+        <h2 class="waving | text-5xl text-invers mb-16 font-normal font-title">
+            404
+        </h2>
+
+        <div class="text-right">
+            <blockquote class="text-5xl text-left | my-8">
+                <a href="/" class="text-accent">Return</a> from whence thou cam'st for that is thy place of belonging.
+            </blockquote>
+            <cite class="text-right">&dash; Sister Friede, Dark Souls 3</cite>
+        </div>
+    </div>
+</main>
+@endsection
